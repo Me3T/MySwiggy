3539a27f1d45a0a0a0c4cd87f77c1e9d2d56341d
Me3T <rai.manmeet1998@gmail.com>
2025-03-05 14:01:53 +0530

    Built Dynamic Components

--- App.js
+++ App.js
@@ -15,6 +15,24 @@ import ReactDOM from "react-dom/client";
  * - Contact
  */
 
+const resObj = [
+  {
+    name: "Pizza Hut",
+    image:
+      "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/57dcac8c-db09-4aec-9893-b7a60abc5669_119514.jpg",
+    rating: 4.1,
+    cuisines: ["Pizza", "Fast Food"],
+    time: "20-25 mins",
+  },
+  {
+    name: "Khatu Ji Vaishnu Dhaba",
+    image: "oshmkqy9evxn4v8zaapc",
+    rating: 4.6,
+    cuisines: ["North Indian", "South Indian"],
+    time: "15-20 mins",
+  },
+];
+
 const Header = () => {
   return (
     <div className="header">
@@ -41,18 +59,23 @@ const styleCard = {
   backgroundColor: "#f0f0f0",
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const { name, cuisines, rating, time } = resData;
   return (
     <div className="res-card" style={styleCard}>
       <img
         className="res-logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/57dcac8c-db09-4aec-9893-b7a60abc5669_119514.jpg"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData.image
+        }
         alt="res-logo"
       />
-      <h3>Gullu Da Dhaba</h3>
-      <h4>Pizza , Garlic Bread</h4>
-      <h4>4.4 stars</h4>
-      <h4>16 minutes</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{rating} stars</h4>
+      <h4>{time} minutes</h4>
     </div>
   );
 };
@@ -62,14 +85,12 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        {/* <RestaurantCard resData={resObj[0]} />
+      
+        <RestaurantCard resData={resObj[1]} /> */}
+        {resObj.map((restaurant) => (
+          <RestaurantCard resData={restaurant} />
+        ))}
       </div>
     </div>
   );
