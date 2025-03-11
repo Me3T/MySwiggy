227634719eb07160d62d9285a787dbe3a377125c
Me3T <rai.manmeet1998@gmail.com>
2025-03-11 21:48:42 +0530

    mockData

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -5,7 +5,7 @@ export const resObj = [
     image:
       "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/57dcac8c-db09-4aec-9893-b7a60abc5669_119514.jpg",
     rating: 4.1,
-    cuisines: ["Pizza", "Fast Food"],
+    cuisines: ["Pizza", "Fast Food", "Garlic Bread"],
     time: "20-25 mins",
   },
   {
